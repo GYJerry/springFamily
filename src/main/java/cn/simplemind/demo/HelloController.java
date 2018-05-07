5aa67f3fdda99b18079fafbb57eab20182ea237f
Jerry <JerryWu1993@gmail.com>
2018-05-07 20:51:06 +0800

    project init

---
+++ src/main/java/cn/simplemind/demo/HelloController.java
@@ -0,0 +1,12 @@
+package cn.simplemind.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("hello")
+	public Object hello() {
+		return "Hello springboot";
+	}
+}
